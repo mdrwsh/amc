3338fc8a5900e31430833021b47b707a83174b97
M. Darwish <dxrwish0201@gmail.com>
2022-10-11 21:55:40 +0800

    add recent, optimize code

--- amc.py
+++ amc.py
@@ -22,8 +22,10 @@ if not path.exists("amc.log"): makedirs("amc.log")
 data = [i.split(',') for i in open("list.txt","r").read().split('\n')]
 if data[-1] == ['']: data.pop(-1)
 last = data[0][0]
+current = str(date.today())
 data.pop(0)
 text = ''
+recent = 'N/A'
 
 in_list = []
 out_list = []
@@ -31,13 +33,13 @@ for i in data:
     if i[1] == "IN": in_list.append(i[0])
     else: out_list.append(i[0]+'\n')
 
-if str(date.today()) != last:
+if current != last:
     if len(out_list) > 0:
         with open("amc.log/"+last+".txt", "w") as f:
             f.writelines(out_list)
         data = [[i[0],"IN"] for i in data]
         
-wdata = [str(date.today())+'\n'] + [','.join(i)+'\n' for i in data]
+wdata = [current+'\n'] + [','.join(i)+'\n' for i in data]
 with open("list.txt","w") as f:
     f.writelines(wdata)
 text = ''
@@ -50,7 +52,7 @@ while True:
         
     system("cls" if os == "nt" else "clear")
     print("\033[90mPress 0 to VIEW\n")
-    print(f"\033[32mIN: {c_in}\n\033[93mOUT: {c_out}\033[0m\n\nSearch:{text}|\n")
+    print(f"\033[32mIN: {c_in}\n\033[93mOUT: {c_out}\033[90m\n\nRecent: {recent}\033[0m\n\nSearch:{text}|\n")
     
     if text == '': result = []
     if result != []:
@@ -68,7 +70,6 @@ while True:
         for i in data:
             if i[1] == "IN": in_list.append(i[0])
             else: out_list.append(i[0])
-        system("cls" if os == "nt" else "clear")
         print("\033[32mIN:")
         for n,i in enumerate(in_list): print(str(n+1) + ". " + i)
         print("\n\033[93mOUT:")
@@ -81,8 +82,10 @@ while True:
         char = int(char)
         if len(result) > 0:
             if char <= len(result):
+                print("\n\033[0mUpdating...")
                 data[result[char-1][1]][1] = "OUT" if data[result[char-1][1]][1] == "IN" else "IN"
-                wdata = [str(date.today())+'\n'] + [','.join(i)+'\n' for i in data]
+                recent = ' --> '.join(data[result[char-1][1]])
+                wdata = [current+'\n'] + [','.join(i)+'\n' for i in data]
                 with open("list.txt","w") as f:
                     f.writelines(wdata)
                 text = ''
