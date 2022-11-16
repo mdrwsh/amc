154e69accde22b9f152ba8fea121d24a7735fbe2
M. Darwish <dxrwish0201@gmail.com>
2022-11-16 22:23:55 +0800

    better search, little bit config implementation

--- amc.py
+++ amc.py
@@ -1,8 +1,19 @@
 from os import system, path, makedirs, name as os
 from datetime import date, timedelta
 
+reload = True
 print("Starting...")
-    
+
+if not path.exists("amc.log"): makedirs("amc.log")
+keyVal = {
+          'g':0,'h':0,
+          't':-1,'y':1,'u':1,'j':1,'n':-1,'b':-1,'v':-1,'f':-1,
+          'r':-2,'d':-2,'c':-2,'i':2,'k':2,'m':-2,
+          'e':-3,'s':-3,'x':-3,'o':3,'l':3,
+          'w':-4,'a':-4,'z':-4,
+          'p':5,'q':-5, ' ':-1
+          }
+
 try:
     import msvcrt
     def getChar():
@@ -19,44 +30,60 @@ except ImportError:
             termios.tcsetattr(fd, termios.TCSADRAIN, oldSettings)
         return answer
 
-if not path.exists("amc.log"): makedirs("amc.log")
-
-keyVal = {
-  'g':0,'h':0,
-  't':-1,'y':1,'u':1,'j':1,'n':-1,'b':-1,'v':-1,'f':-1,
-  'r':-2,'d':-2,'c':-2,'i':2,'k':2,'m':-2,
-  'e':-3,'s':-3,'x':-3,'o':3,'l':3,
-  'w':-4,'a':-4,'z':-4,
-  'p':5,'q':-5, ' ':-1
-  }
-  
-data = [i.split(',') for i in open("list.txt","r").read().split('\n')]
-if data[-1] == ['']: data.pop(-1)
-last = data[0][0]
-current = str(date.today())
-data.pop(0)
-names = [i[0].lower() for i in data]
-text = ''
-recent = 'N/A'
-
-in_list = []
-out_list = []
-for i in data:
-    if i[1] == "IN": in_list.append(i[0])
-    else: out_list.append(i[0]+'\n')
-
-if current != last:
-    if len(out_list) > 0:
-        with open("amc.log/"+last+".txt", "w") as f:
-            f.writelines(out_list)
-        data = [[i[0],"IN"] for i in data]
+def config():
+    database = [i.split(',') for i in open("database.txt","r").read().split('\n')]
+    if database[-1] == ['']: database.pop(-1)
+    text = ''
+    whitelist = []
+    for i,_ in database:
+        if i.lower() in names:
+            whitelist.append(i)
+    while True:
+        system("cls" if os == "nt" else "clear")
+        print(f"Search:{text}\n\n")
+        if text == '':
+            for n,i in whitelist: print(f"{n}. {i}")
+        else:
+            for n,i in result: print(f"{n}. {i}")
+        char = getChar()
+        if char.isdigit():
+            pass
+        elif char.isalpha() or char in [" ","\x7f","\x08"]:
+            if char in ['\x7f','\x08']: text = text[:-1]
+            else: text += char.lower()
+            result = []
         
-wdata = [current+'\n'] + [','.join(i)+'\n' for i in data]
-with open("list.txt","w") as f:
-    f.writelines(wdata)
-text = ''
-    
 while True:
+    if reload == True:
+    
+        data = [i.split(',') for i in open("list.txt","r").read().split('\n')]
+        if data[-1] == ['']: data.pop(-1)
+        last = data[0][0]
+        current = str(date.today())
+        data.pop(0)
+        names = [i[0].lower() for i in data]
+        text = ''
+        recent = 'N/A'
+
+        in_list = []
+        out_list = []
+        for i in data:
+            if i[1] == "IN": in_list.append(i[0])
+            else: out_list.append(i[0]+'\n')
+
+        if current != last:
+            if len(out_list) > 0:
+                with open("amc.log/"+last+".txt", "w") as f:
+                    f.writelines(out_list)
+                data = [[i[0],"IN"] for i in data]
+                
+        wdata = [current+'\n'] + [','.join(i)+'\n' for i in data]
+        with open("list.txt","w") as f:
+            f.writelines(wdata)
+        text = ''
+        reload = False
+
+    # MAIN
     c_in = c_out = 0
     for _,i in data:
         if i == "IN": c_in += 1
@@ -74,8 +101,9 @@ while True:
             print(f"{n+1}. {i[0]}")
             
     char = getChar()
-    
-    if char == "0":
+
+    if char == ")": config()
+    elif char == "0":
         system("cls" if os == "nt" else "clear")
         in_list = []
         out_list = []
@@ -108,22 +136,25 @@ while True:
         prob = []
         for name in names:
             subname = []
-            for word in name.split():
-                if word[0] == "(": break
-                similarity = 100
-                if not text in word:
-                    similarity -= 2*abs(len(text)-len(word))
-                    for i in range(min(len(text),len(word))):
-                        try: similarity -= 5*abs(keyVal[text[i]]-keyVal[word[i]])
-                        except: pass
-                        try: similarity -= 5*abs(keyVal[text[-(i+1)]]-keyVal[word[-(i+1)]])
-                        except: pass
-                subname.append(similarity)
+            # TODO: ignore multiple spaces
+            if text in name: subname = [100]
+            else:
+                for word in name.split():
+                    if word[0] == "(": break
+                    similarity = 100
+                    if not text in word:
+                        similarity -= 2*abs(len(text)-len(word))
+                        for i in range(min(len(text),len(word))):
+                            try: similarity -= 5*abs(keyVal[text[i]]-keyVal[word[i]])
+                            except: pass
+                            try: similarity -= 5*abs(keyVal[text[-(i+1)]]-keyVal[word[-(i+1)]])
+                            except: pass
+                    subname.append(similarity)
             prob.append(max(subname))
         result = sorted(zip(prob,range(len(data))), reverse=True)[:9]
-        for n,i in enumerate(result):
-            if i[0] < 60:
-                result = result[:n]
-                break
-        # result = [(names[i[1]]+f" ({i[0]}%)",i[1]) for i in result]
-        result = [(names[i[1]],i[1]) for i in result]
+        # for n,i in enumerate(result):
+            # if i[0] < 60:
+                # result = result[:n]
+                # break
+        result = [(names[i[1]]+f" ({i[0]}%)",i[1]) for i in result]
+        # result = [(names[i[1]],i[1]) for i in result]
